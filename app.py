1353f6800a6b2598b67aa407787eb6566ff06a2d
Himanshu1198 <himanshu.main.work@gmail.com>
2025-12-11 23:25:25 +0530

    final

--- app.py
+++ app.py
@@ -1158,5 +1158,5 @@ def health_check():
     return jsonify({'status': 'ok'}), 200
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", debug=True, port=5000)
 
